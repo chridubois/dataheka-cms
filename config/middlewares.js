9ac2038e6f9362c6be65ac60613ee8a2a5223f07
Christophe Dubois <idategto11@gmail.com>
2024-01-12 08:50:48 +0100

    update security policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,27 @@
+const BUCKET = process.env.YANDEX_CLOUD_BUCKET;
+const BUCKET_URL = `https://${BUCKET}.storage.yandexcloud.net`;
+
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', BUCKET_URL],
+          'media-src': ["'self'", 'data:', 'blob:', BUCKET_URL],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
